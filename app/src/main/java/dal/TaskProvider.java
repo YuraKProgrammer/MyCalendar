fe3200bb5a1eee7e90a828314ab354f6116261c4
YuraKProgrammer <yura.kalantyr@mail.ru>
2022-04-09 11:03:54 +0300

    Добавлено сохранение задач в память

--- app/src/main/java/dal/TaskProvider.java
+++ app/src/main/java/dal/TaskProvider.java
@@ -4,19 +4,26 @@ import android.content.Context;
 import models.Date;
 import models.Task;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TaskProvider implements ITaskProvider{
-    private Task[] tasks = new Task[1];
+    private Task[] tasks = new Task[0];
 
     Context context;
 
     public TaskProvider(Context context){
-        tasks[0]=new Task(new Date(2000, 1, 1),"Новый план",1);
         this.context=context;
+        try {
+            InputStream fileStream = context.openFileInput("tasks.bin");
+            byte[] bytes = new byte[fileStream.available()];
+            fileStream.read(bytes);
+            tasks = deserializeTasks(bytes);
+        }
+        catch (Exception e){
+            tasks = new Task[0];
+        }
     }
 
     public Task[] getTasks(Date date){
@@ -36,22 +43,28 @@ public class TaskProvider implements ITaskProvider{
         tasks = Arrays.stream(tasks)
                 .filter(t -> t.getId()!=taskId)
                 .toArray(Task[]::new);
+        saveToFile();
     }
 
     @Override
     public void addTask(Task task) {
         ArrayList<Task> list = new ArrayList<>(Arrays.asList(tasks));
-        int maxId = Arrays.stream(tasks)
-                .mapToInt(Task::getId)
-                .max()
-                .getAsInt();
+        int maxId=0;
+        if (list.size()>0) {
+            maxId = Arrays.stream(tasks)
+                    .mapToInt(Task::getId)
+                    .max()
+                    .getAsInt();
+        }
         task.setId(maxId+1);
         list.add(task);
         tasks=list.toArray(tasks);
+    }
 
-        String filename = "file";
+    private void saveToFile() {
+        String filename = "tasks.bin";
         try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE)) {
-            fos.write(null);
+            fos.write(serializeTasks(tasks));
         }
         catch (Exception e){
             System.out.println(e);
@@ -60,5 +73,28 @@ public class TaskProvider implements ITaskProvider{
 
     @Override
     public void updateTask(int id) {
+        saveToFile();
+    }
+
+    public static byte[] serializeTasks(Task[] tasks) throws IOException {
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        ObjectOutputStream outputStream = new ObjectOutputStream(byteStream);
+        outputStream.writeInt(tasks.length);
+        for (int i=0; i<tasks.length; i++) {
+            outputStream.writeObject(tasks[i]);
+        }
+        outputStream.flush();
+        return byteStream.toByteArray();
+    }
+
+    public static Task[] deserializeTasks(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteStream = new ByteArrayInputStream(bytes);
+        ObjectInputStream inputStream = new ObjectInputStream(byteStream);
+        int count = inputStream.readInt();
+        Task[] tasks = new Task[count];
+        for (int i=0; i<count; i++){
+            tasks[i]=(Task)inputStream.readObject();
+        }
+        return tasks;
     }
 }
--- app/src/test/java/com/yura/mycalendar/TaskProviderTest.java
+++ app/src/test/java/com/yura/mycalendar/TaskProviderTest.java
@@ -6,5 +6,18 @@ import models.Task;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.*;
+
 public class TaskProviderTest {
+    @Test
+    public void serializeTasks_test() throws IOException, ClassNotFoundException {
+        Task t1 = new Task(new Date(1999,9,8),"task",1);
+        Task t2 = new Task(new Date(2005,1,15),"task2",2);
+        Task[] tasks = new Task[]{t1,t2};
+        byte[] b = TaskProvider.serializeTasks(tasks);
+        Task[] tasks2 = TaskProvider.deserializeTasks(b);
+        for (int i=0; i<tasks.length; i++) {
+            Assert.assertTrue(Task.areEquals(tasks[i],tasks2[i]));
+        }
+    }
 }
